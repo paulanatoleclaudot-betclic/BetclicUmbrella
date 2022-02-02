630fb91b818d4e8c92e479057f2c674da3002e95
Paul-Anatole CLAUDOT <p.claudot@betclicgroup.com>
2022-02-02 16:44:52 +0100

    Add TrustCommander

--- Package.swift
+++ Package.swift
@@ -9,13 +9,37 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-      .library(
-        name: "FraudForce",
-        targets: ["FraudForce"])
+        .library(
+            name: "TCCore",
+            targets: ["TCCore"]),
+        .library(
+            name: "TCIAB",
+            targets: ["TCIAB"]),
+        .library(
+            name: "TCPrivacy",
+            targets: ["TCPrivacy"]),
+        .library(
+            name: "TCSDK",
+            targets: ["TCSDK"]),
+        .library(
+            name: "FraudForce",
+            targets: ["FraudForce"])
     ],
     targets: [
-      .binaryTarget(
-        name: "FraudForce",
-        path: "./Sources/FraudForce/FraudForce.xcframework")
+        .binaryTarget(
+            name: "TCCore",
+            path: "./Sources/TCCore/TCCore.xcframework"),
+        .binaryTarget(
+            name: "TCIAB",
+            path: "./Sources/TCIAB/TCIAB.xcframework"),
+        .binaryTarget(
+            name: "TCPrivacy",
+            path: "./Sources/TCPrivacy/TCPrivacy.xcframework"),
+        .binaryTarget(
+            name: "TCSDK",
+            path: "./Sources/TCSDK/TCSDK.xcframework"),
+        .binaryTarget(
+            name: "FraudForce",
+            path: "./Sources/FraudForce/FraudForce.xcframework")
     ]
 )
