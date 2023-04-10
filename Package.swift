8bd3ffe1a874780ba500164315bbcd638175e4cb
Paul-Anatole CLAUDOT <p.claudot@betclicgroup.com>
2023-04-10 21:23:01 +0200

    Only places

--- Package.swift
+++ Package.swift
@@ -10,40 +10,10 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "TCCore",
-            targets: ["TCCore"]),
-        .library(
-            name: "TCIAB",
-            targets: ["TCIAB"]),
-        .library(
-            name: "TCPrivacy",
-            targets: ["TCPrivacy"]),
-        .library(
-            name: "TCSDK",
-            targets: ["TCSDK"]),
-        .library(
-            name: "FraudForce",
-            targets: ["FraudForce"]),
-        .library(
             name: "GooglePlaces",
             targets: ["GooglePlaces"])
     ],
     targets: [
-        .binaryTarget(
-            name: "TCCore",
-            path: "./Sources/TCCore/TCCore.xcframework"),
-        .binaryTarget(
-            name: "TCIAB",
-            path: "./Sources/TCIAB/TCIAB.xcframework"),
-        .binaryTarget(
-            name: "TCPrivacy",
-            path: "./Sources/TCPrivacy/TCPrivacy.xcframework"),
-        .binaryTarget(
-            name: "TCSDK",
-            path: "./Sources/TCSDK/TCSDK.xcframework"),
-        .binaryTarget(
-            name: "FraudForce",
-            path: "./Sources/FraudForce/FraudForce.xcframework"),
         .binaryTarget(
             name: "GooglePlaces",
             path: "./Sources/GooglePlaces/GooglePlaces.xcframework")
